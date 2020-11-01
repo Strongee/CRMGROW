cf0472d26d313c2b1aaded250206f53781fe10ea
Sapphire8888 <millionsapphire@outlook.com>
2020-11-01 20:51:30 +0800

    add tag manager

--- src/app/pages/tag-manager/tag-manager.component.ts
+++ src/app/pages/tag-manager/tag-manager.component.ts
@@ -6,10 +6,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tag-manager.component.scss']
 })
 export class TagManagerComponent implements OnInit {
+  tags = [
+    {
+      label: 'hot',
+      times: 10,
+      names: [
+        { short: 'PN', full: 'Przemyslaw Nowakowski' },
+        { short: 'TA', full: 'Thomas Anderson' },
+        { short: 'RB', full: 'Rafal Baran' },
+        { short: 'NC', full: 'Nick Carter' },
+        { short: 'JW', full: 'Jiang Wang' },
+        { short: 'DJ', full: 'Da Jin' },
+        { short: 'ML', full: 'Mori Lucas' },
+        { short: 'DJ', full: 'David Johnson' },
+        { short: 'HK', full: 'Harry Kane' },
+        { short: 'JJ', full: 'John Jakaive' }
+      ]
+    },
+    {
+      label: 'interested',
+      times: 4,
+      names: [
+        { short: 'PN', full: 'Przemyslaw Nowakowski' },
+        { short: 'TA', full: 'Thomas Anderson' },
+        { short: 'RB', full: 'Rafal Baran' },
+        { short: 'NC', full: 'Nick Carter' }
+      ]
+    },
+    {
+      label: '2019Q4',
+      times: 5,
+      names: [
+        { short: 'PN', full: 'Przemyslaw Nowakowski' },
+        { short: 'TA', full: 'Thomas Anderson' },
+        { short: 'RB', full: 'Rafal Baran' },
+        { short: 'NC', full: 'Nick Carter' },
+        { short: 'DJ', full: 'David Johnson' }
+      ]
+    }
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
