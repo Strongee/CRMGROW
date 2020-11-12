c98a071b9a2afe4dff90cf738896b414b799380f
Sapphire8888 <millionsapphire@outlook.com>
2020-11-12 11:57:14 +0800

    add custom field delete, update lead capture

--- src/app/components/custom-field-add/custom-field-add.component.ts
+++ src/app/components/custom-field-add/custom-field-add.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-custom-field-add',
@@ -7,7 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomFieldAddComponent implements OnInit {
   fieldName = '';
-  constructor() {}
+  typeOption = 'text';
+  suggestion = '';
+  option_id = 1;
+  options = [{ name: 'Select 1', id: 'option1' }];
+  constructor(private dialogRef: MatDialogRef<CustomFieldAddComponent>) {}
 
   ngOnInit(): void {}
+
+  addField(): void {
+    if (this.typeOption == 'text') {
+      const data = {
+        field: this.fieldName,
+        placeholder: this.suggestion,
+        mode: 'text'
+      };
+      this.dialogRef.close(data);
+    } else {
+      const data = {
+        options: this.options,
+        mode: 'dropdown'
+      };
+      this.dialogRef.close(data);
+    }
+  }
+
+  addOption(): void {
+    this.option_id++;
+    const data = {
+      name: 'Select 1',
+      id: 'option' + this.option_id
+    };
+    this.options.push(data);
+  }
+
+  deleteOption(optionId: string): void {
+    const options = this.options.filter((option) => option.id != optionId);
+    this.options = [];
+    this.options = options;
+  }
 }
