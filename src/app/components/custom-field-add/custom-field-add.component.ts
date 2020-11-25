2507d4f34c79ca9e445766bb087a8445128b384a
Sapphire8888 <millionsapphire@outlook.com>
2020-11-26 04:21:55 +0800

    update lead capture

--- src/app/components/custom-field-add/custom-field-add.component.ts
+++ src/app/components/custom-field-add/custom-field-add.component.ts
@@ -25,19 +25,17 @@ export class CustomFieldAddComponent implements OnInit {
 
   ngOnInit(): void {
     if (this.data) {
+      console.log('##', this.data);
       this.customFieldType = 'update';
-      this.fieldName = this.data.field.field_name;
+      this.fieldName = this.data.field.name;
       if (this.data.field.placeholder) {
         this.suggestion = this.data.field.placeholder;
         this.typeOption = 'text';
       }
-      if (this.data.field.options) {
+      if (this.data.field.options.length > 0) {
         this.options = this.data.field.options;
         this.typeOption = 'dropdown';
       }
-      this.customFieldType = 'update';
-      this.fieldName = this.data.field.field_name;
-      this.suggestion = this.data.field.placeholder;
     } else {
       this.customFieldType = 'create';
     }
