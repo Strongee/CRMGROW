110b84d65ab14bd2e1746494a91ee67587cd9f9f
Sapphire8888 <millionsapphire@outlook.com>
2020-12-22 04:17:25 +0800

    update mobile responsive, deal loading bar

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   // api: 'https://app.crmgrow.com/api/',
-  api: 'http://localhost:3000/api/',
-  // api: 'https://app.crmgrow.com/api/',
+  // api: 'http://localhost:3000/api/',
+  api: 'https://app.crmgrow.com/api/',
   server: 'https://app.crmgrow.com',
   website: 'https://app.crmgrow.com',
   ClientId: {
