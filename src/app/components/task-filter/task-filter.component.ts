5e569fdc1035940216d92ddba7c49a76cd56c781
LOMFM <brightdawn019@gmail.com>
2020-12-24 09:13:45 +0600

    task filter update

--- src/app/components/task-filter/task-filter.component.ts
+++ src/app/components/task-filter/task-filter.component.ts
@@ -1,6 +1,8 @@
 import { SelectionModel } from '@angular/cdk/collections';
 import { Component, OnInit } from '@angular/core';
 import { TaskStatus, TIMES } from 'src/app/constants/variable.constants';
+import { Contact } from 'src/app/models/contact.model';
+import { TaskSearchOption } from 'src/app/models/searchOption.model';
 import { LabelService } from 'src/app/services/label.service';
 import { TabOption } from 'src/app/utils/data.types';
 
@@ -21,8 +23,11 @@ export class TaskFilterComponent implements OnInit {
   types = [];
   status = this.STATUS_OPTIONS[1];
   selectedLabels = new SelectionModel<string>(true, []);
+  contact;
   startDate;
   startTime;
+  endDate;
+  endTime;
   loading = false;
 
   constructor(public labelService: LabelService) {}
@@ -38,8 +43,27 @@ export class TaskFilterComponent implements OnInit {
     }
   }
 
+  selectContact(event: Contact): void {
+    this.contact = event._id;
+  }
+
   /**
    * Apply Filter to the Task List
    */
-  applyFilters(): void {}
+  applyFilters(): void {
+    const searchOption = new TaskSearchOption();
+    searchOption.str = this.search;
+    let status;
+    if (this.status.value === TaskStatus.TODO) {
+      status = 0;
+    } else if (this.status.value == TaskStatus.COMPLETED) {
+      status = 1;
+    }
+    searchOption.status = status;
+    searchOption.contact = this.contact;
+    searchOption.types = this.types;
+    searchOption.labels = this.selectedLabels.selected;
+
+    // Timezone
+  }
 }
