647b67e05fd6b3e649fa1ce345ae7d2071df708f
Sapphire8888 <millionsapphire@outlook.com>
2021-01-20 00:16:53 +0800

    update deal setting

--- src/app/pages/deals-setting/deals-setting.component.ts
+++ src/app/pages/deals-setting/deals-setting.component.ts
@@ -4,6 +4,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { DealStage } from 'src/app/models/deal-stage.model';
 import { DealStageCreateComponent } from 'src/app/components/deal-stage-create/deal-stage-create.component';
 import { DealStageDeleteComponent } from 'src/app/components/deal-stage-delete/deal-stage-delete.component';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-deals-setting',
@@ -11,11 +12,25 @@ import { DealStageDeleteComponent } from 'src/app/components/deal-stage-delete/d
   styleUrls: ['./deals-setting.component.scss']
 })
 export class DealsSettingComponent implements OnInit {
+  stages: any[] = [];
+
   constructor(private dialog: MatDialog, public dealsService: DealsService) {
     this.dealsService.getStage(true);
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.dealsService.stages$.subscribe((res) => {
+      this.stages = [...this.stages, ...res];
+    });
+  }
+
+  drop(event: CdkDragDrop<string[]>): void {
+    moveItemInArray(
+      event.container.data,
+      event.previousIndex,
+      event.currentIndex
+    );
+  }
 
   moveDelete(id: string): void {
     this.dialog
