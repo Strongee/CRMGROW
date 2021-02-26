3b3baece693c907e5f6718f22cd8ac9c52a3e769
IndigoSnake <indigo.snake@yandex.com>
2021-02-27 00:13:43 +0800

    implement contact detail dialog for group call

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -47,7 +47,6 @@ import { DealsDetailComponent } from '../../pages/deals-detail/deals-detail.comp
 import { AutoflowComponent } from '../../pages/autoflow/autoflow.component';
 import { VideoCreateComponent } from '../../pages/video-create/video-create.component';
 import { MoneyPipe } from '../../pipes/money.pipe';
-import { TimeDurationPipe } from '../../pipes/time-duration.pipe';
 import { CampaignListItemComponent } from '../../pages/campaign-list-item/campaign-list-item.component';
 import { CampaignBulkMailingItemComponent } from '../../pages/campaign-bulk-mailing-item/campaign-bulk-mailing-item.component';
 import { AutoResendVideoComponent } from '../../pages/auto-resend-video/auto-resend-video.component';
@@ -107,7 +106,6 @@ import { MessagesComponent } from 'src/app/pages/messages/messages.component';
     AutoflowComponent,
     VideoCreateComponent,
     MoneyPipe,
-    TimeDurationPipe,
     CampaignListItemComponent,
     CampaignBulkMailingItemComponent,
     ContactComponent,
