942c30f4b39c285f7f72cb9d305fb48074d9b142
Sapphire8888 <millionsapphire@outlook.com>
2021-04-19 02:52:45 +0800

    update billing page

---
+++ src/app/components/payment-card/payment-card.component.ts
@@ -0,0 +1,114 @@
+import { Component, Inject, NgZone, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MONTH, YEAR } from 'src/app/constants/variable.constants';
+import { UserService } from 'src/app/services/user.service';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-payment-card',
+  templateUrl: './payment-card.component.html',
+  styleUrls: ['./payment-card.component.scss']
+})
+export class PaymentCardComponent implements OnInit {
+  saving = false;
+  MONTHS = MONTH;
+  YEARS = YEAR;
+  card = {
+    card_name: '',
+    number: '',
+    cvc: '',
+    exp_year: '',
+    exp_month: '',
+    card_brand: '',
+    last4: '',
+    plan_id: ''
+  };
+  // Card Number Length
+  cardNumberLen = 16;
+  // Card Number Input Setting
+  creditCardInput = {
+    creditCard: true,
+    onCreditCardTypeChanged: (type) => {
+      this.card.card_brand = type;
+      switch (type) {
+        case 'amex':
+          this.cardNumberLen = 15;
+          break;
+        case 'visa':
+        case 'mastercard':
+        case 'jcb':
+        case 'discover':
+          this.cardNumberLen = 16;
+          break;
+        case 'diners':
+          this.cardNumberLen = 14;
+          break;
+        default:
+          this.cardNumberLen = 16;
+      }
+    }
+  };
+  // Card CVC Input Setting
+  credCvcInput = {
+    numeral: true,
+    numeralThousandsGroupStyle: 'wan'
+  };
+
+  constructor(
+    private dialogRef: MatDialogRef<PaymentCardComponent>,
+    @Inject(MAT_DIALOG_DATA) private data: any,
+    private userService: UserService,
+    private ngZone: NgZone,
+    private toast: ToastrService
+  ) {
+    if (this.data && this.data.card) {
+      this.card = { ...this.data.card };
+    }
+  }
+
+  ngOnInit(): void {}
+
+  changeLast4Code(): void {
+    const number = this.card.number.replace(' ', '');
+    this.card.last4 = number.substr(number.length - 4, 4);
+  }
+
+  editPayment(): void {
+    this.saving = true;
+    (<any>window).Stripe.card.createToken(
+      {},
+      (status: number, response: any) => {
+        if (status === 200) {
+          this.ngZone.run(() => {
+            this.userService
+              .updatePayment({
+                token: { ...response, card_name: this.card.card_brand },
+                plan_id: this.card.plan_id
+              })
+              .subscribe(
+                () => {
+                  this.saving = false;
+                  this.toast.success(
+                    'Your Billing Information is updated successfully.'
+                  );
+                  this.dialogRef.close(this.card);
+                },
+                () => {
+                  this.saving = false;
+                  this.dialogRef.close();
+                }
+              );
+          });
+        } else {
+          this.ngZone.run(() => {
+            this.saving = false;
+            this.toast.error(
+              'Your card information is not correct. Please try again.'
+            );
+            this.dialogRef.close();
+          });
+        }
+      }
+    );
+  }
+}
--- src/app/pages/payment/payment.component.ts
+++ src/app/pages/payment/payment.component.ts
@@ -1,9 +1,9 @@
-import { Component, NgZone, OnDestroy, OnInit } from '@angular/core';
-import { MONTH, YEAR } from '../../constants/variable.constants';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user.model';
 import { UserService } from 'src/app/services/user.service';
-import { ToastrService } from 'ngx-toastr';
 import { Subscription } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { PaymentCardComponent } from 'src/app/components/payment-card/payment-card.component';
 
 @Component({
   selector: 'app-payment',
@@ -12,56 +12,17 @@ import { Subscription } from 'rxjs';
 })
 export class PaymentComponent implements OnInit, OnDestroy {
   // UI Variables;
-  MONTHS = MONTH;
-  YEARS = YEAR;
   loading = false;
   // New Card Information
   card = {
-    name: '',
+    card_name: '',
     number: '',
     cvc: '',
     exp_year: '',
     exp_month: '',
     card_brand: '',
-    last4: ''
-  };
-  // Current Payment Information
-  payment = {
-    card_brand: '',
-    exp_month: '',
-    exp_year: '',
     last4: '',
     plan_id: ''
-  };
-  // Card Number Length
-  cardNumberLen = 16;
-  // Card Number Input Setting
-  creditCardInput = {
-    creditCard: true,
-    onCreditCardTypeChanged: (type) => {
-      this.card.card_brand = type;
-      switch (type) {
-        case 'amex':
-          this.cardNumberLen = 15;
-          break;
-        case 'visa':
-        case 'mastercard':
-        case 'jcb':
-        case 'discover':
-          this.cardNumberLen = 16;
-          break;
-        case 'diners':
-          this.cardNumberLen = 14;
-          break;
-        default:
-          this.cardNumberLen = 16;
-      }
-    }
-  };
-  // Card CVC Input Setting
-  credCvcInput = {
-    numeral: true,
-    numeralThousandsGroupStyle: 'wan'
   };
 
   invoices = [
@@ -88,11 +49,7 @@ export class PaymentComponent implements OnInit, OnDestroy {
 
   profileSubscription: Subscription;
 
-  constructor(
-    private userService: UserService,
-    private ngZone: NgZone,
-    private toast: ToastrService
-  ) {
+  constructor(private userService: UserService, private dialog: MatDialog) {
     this.loading = true;
     this.profileSubscription && this.profileSubscription.unsubscribe();
     this.profileSubscription = this.userService.profile$.subscribe(
@@ -101,11 +58,11 @@ export class PaymentComponent implements OnInit, OnDestroy {
           this.userService.getPayment(profile.payment).subscribe(
             (res) => {
               this.loading = false;
-              this.payment = res;
               this.card = {
                 ...res,
-                number: this.payment.last4
+                number: res.last4
               };
+              this.getInvoice();
             },
             () => {
               this.loading = false;
@@ -124,51 +81,35 @@ export class PaymentComponent implements OnInit, OnDestroy {
     this.profileSubscription && this.profileSubscription.unsubscribe();
   }
 
-  changeLast4Code(): void {
-    const number = this.card.number.replace(' ', '');
-    this.card.last4 = number.substr(number.length - 4, 4);
-  }
-  createBill(): void {
-    this.saving = true;
-    (<any>window).Stripe.card.createToken(
-      this.card,
-      (status: number, response: any) => {
-        if (status === 200) {
-          this.ngZone.run(() => {
-            this.userService
-              .updatePayment({
-                token: { ...response, card_name: this.payment.card_brand },
-                plan_id: this.payment.plan_id
-              })
-              .subscribe(
-                () => {
-                  this.saving = false;
-                  this.toast.success(
-                    'Your Billing Information is updated successfully.'
-                  );
-                },
-                () => {
-                  this.saving = false;
-                }
-              );
-          });
-        } else {
-          this.ngZone.run(() => {
-            this.saving = false;
-            this.toast.error(
-              'Your card information is not correct. Please try again.'
-            );
-          });
-        }
-      }
-    );
+  getInvoice(): void {
+    this.userService.getInvoice().subscribe((res) => {
+      console.log('###', res);
+    });
   }
 
-  cancelBill(): void {
-    this.card.number = this.payment.last4;
-    this.card.card_brand = this.payment.card_brand;
-    this.card.exp_month = this.card.exp_month;
-    this.card.exp_year = this.card.exp_year;
-    this.card.cvc = '';
+  // cancelBill(): void {
+  //   this.card.number = this.payment.last4;
+  //   this.card.card_brand = this.payment.card_brand;
+  //   this.card.exp_month = this.card.exp_month;
+  //   this.card.exp_year = this.card.exp_year;
+  //   this.card.cvc = '';
+  // }
+
+  editCard(): void {
+    this.dialog
+      .open(PaymentCardComponent, {
+        position: { top: '100px' },
+        width: '100vw',
+        maxWidth: '450px',
+        data: {
+          card: this.card
+        }
+      })
+      .afterClosed()
+      .subscribe((res) => {
+        if (res) {
+          this.card = res;
+        }
+      });
   }
 }
