6e944a8d0a8607c4861e19d80b205bcb7b655f6f
Sapphire8888 <millionsapphire@outlook.com>
2021-04-23 03:08:24 +0800

    update sms page

--- src/app/components/purchase-message/purchase-message.component.ts
+++ src/app/components/purchase-message/purchase-message.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { Subscription } from 'rxjs';
+import { User } from 'src/app/models/user.model';
 import { SmsService } from 'src/app/services/sms.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-purchase-message',
@@ -9,29 +12,41 @@ import { SmsService } from 'src/app/services/sms.service';
 })
 export class PurchaseMessageComponent implements OnInit {
   saving = false;
-  currentType = 2;
+  user: User = new User();
   plans = [
     { type: 1, sms: '250', price: '6' },
     { type: 2, sms: '500', price: '10' },
     { type: 3, sms: '1000', price: '15' }
   ];
+  currentType = this.plans[1];
+  profileSubscription: Subscription;
 
   constructor(
     private dialog: MatDialog,
     private dialogRef: MatDialogRef<PurchaseMessageComponent>,
-    private smsService: SmsService
-  ) {}
+    private smsService: SmsService,
+    private userService: UserService
+  ) {
+    this.profileSubscription && this.profileSubscription.unsubscribe();
+    this.profileSubscription = this.userService.profile$.subscribe(
+      (profile) => {
+        if (profile) {
+          this.user = profile;
+        }
+      }
+    );
+  }
 
   ngOnInit(): void {}
 
   selectPlan(plan: any): void {
-    this.currentType = plan.type;
+    this.currentType = plan;
   }
 
   purchase(): void {
     this.saving = true;
     const data = {
-      option: this.currentType
+      option: this.currentType.type
     };
     this.smsService.buyCredit(data).subscribe(
       (res) => {
