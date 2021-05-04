b7fc6061023e2cfa64dc1c3d60f2b2cf4382f466
Sapphire8888 <millionsapphire@outlook.com>
2021-05-04 19:26:26 +0800

    update record window

--- src/app/components/html-editor/html-editor.component.ts
+++ src/app/components/html-editor/html-editor.component.ts
@@ -136,16 +136,12 @@ export class HtmlEditorComponent implements OnInit {
           this.cdr.detectChanges();
         },
         record: () => {
-          let popup, option;
-          option =
-            'toolbar=no, location=no, directories=no, status=no, location=no';
-          option +=
-            ',menubar=no, scrollbars=no, resizable=no, titlebar=no, addressbar=no';
-          option += ',width=530, height=305';
+          let popup;
+          const option = 'width=530, height=305';
           if (!popup || popup.closed) {
             popup = window.open(
               this.recordUrl + '?' + this.authToken,
-              'record',
+              '',
               option
             );
           } else {
